8f14f353456c6d4cf066df446f481364c4618bb7
Leonardo Hahn <leo_lh324@hotmail.com>
2017-09-09 13:16:09 +0200

    Added some possible comments for fix. Changed perl script. Changed some regexes.

--- src/cc_misc.c
+++ src/cc_misc.c
@@ -21,19 +21,22 @@ yyerror(char const *mensagem)
 void
 main_init(int argc, char **argv)
 {
-    //implemente esta função com rotinas de inicialização, se necessário
-  dict = dict_new();
+    dict = dict_new();
 }
 
 void
 main_finalize(void)
 {
-    //implemente esta função com rotinas de inicialização, se necessário
     int i, l;
     for (i = 0, l = dict->size; i < l; ++i) {
-      if (dict->data[i]) {
-        dict_remove(dict, dict->data[i]->key);
-      }
+        if (dict->data[i]) {
+            // o i aqui representa o hash da hash table. Tu na real está removendo só os primeiros items da
+            // hash-table. Pode acontecer que duas keys diferentes sejam mapeadas para o mesmo hash.
+
+            // Nota que o occupation nao fica zero no final, como deveria.
+            // printf("occupation: %d\n", dict->occupation);
+            dict_remove(dict, dict->data[i]->key);
+        }
     }
     dict_free(dict);
 }
@@ -41,13 +44,16 @@ main_finalize(void)
 void
 comp_print_table(void)
 {
-  //para cada entrada na tabela de símbolos
-  //Etapa 1: chame a função cc_dict_etapa_1_print_entrada
-  //implemente esta função
-  int i, l;
-  for (i = 0, l = dict->size; i < l; ++i) {
-    if (dict->data[i]) {
-      cc_dict_etapa_1_print_entrada(dict->data[i]->key, dict->data[i]->value);
+    //para cada entrada na tabela de símbolos
+    //Etapa 1: chame a função cc_dict_etapa_1_print_entrada
+    //implemente esta função
+    int i, l;
+    for (i = 0, l = dict->size; i < l; ++i) {
+        if (dict->data[i]) {
+            // aqui é o mesmo problema, onde vc está printando o hash somente, mas pode ser que existam varias
+            // keys com o mesmo hash.
+            int *line_number = (int*)dict->data[i]->value;
+            cc_dict_etapa_1_print_entrada(dict->data[i]->key, *line_number);
+        }
     }
-  }
 }
