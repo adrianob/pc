66a9eb82a6478359780ebe7f4a249e9de30a3cc0
Adriano Benin <adriano.benin@gmail.com>
2017-11-10 16:20:17 -0200

    add function var errors

---
+++ include/stack.h
@@ -0,0 +1,21 @@
+#ifndef STACK
+#define STACK
+#include <stdbool.h>
+
+typedef bool (*iterator)(void *);
+
+typedef struct _stack {
+  void *data;
+  struct _stack *next;
+} STACK_T;
+
+STACK_T * initialize_stack(void);
+bool is_empty(STACK_T *head);
+void push(STACK_T **head_ref, void *element);
+void * pop(STACK_T **head_ref);
+void * top(STACK_T *head);
+void for_each(STACK_T **head_ref, iterator iterator);
+void destroy(STACK_T **head_ref);
+int list_size(STACK_T *head);
+
+#endif
---
+++ src/stack.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/stack.h"
+
+STACK_T * initialize_stack(void){
+  return NULL;
+}
+
+bool is_empty(STACK_T *head){
+  return head == NULL;
+}
+
+void * top(STACK_T *head){
+  return (head) ? head->data : NULL;
+}
+
+void * pop(STACK_T **head_ref){
+  STACK_T *node = *head_ref;
+  if (!is_empty(node)) {
+    *head_ref = node->next;
+  }
+  return (node) ? node->data : NULL;
+}
+
+void push(STACK_T **head_ref, void *element){
+  STACK_T *node = (STACK_T *) malloc(sizeof(STACK_T));
+  node->data = element;
+  node->next = *head_ref;
+
+  *head_ref = node;
+}
+
+void for_each(STACK_T **head_ref, iterator iterator){
+  STACK_T **node = head_ref;
+  while(!is_empty(*node)){
+    iterator((*node)->data);
+    pop(node);
+  }
+}
+
+void destroy(STACK_T **head_ref){
+  STACK_T *current = *head_ref;
+  STACK_T *next;
+
+  while(current){
+    next = current->next;
+    free(current->data);
+    free(current);
+    current = next;
+  }
+  *head_ref = NULL;
+}
+
+int list_size(STACK_T *head){
+  unsigned int i = 0;
+  STACK_T *node = head;
+  while(node){
+    i++;
+    node = node->next;
+  }
+  return i;
+}
